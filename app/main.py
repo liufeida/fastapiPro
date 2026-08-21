c8b2894b29010ae6fd5351fe4c9e296853465956
liufeida <liufeida@shgbit.com>
2026-08-21 15:59:01 +0800

    feat: op

--- app/main.py
+++ app/main.py
@@ -54,6 +54,7 @@ app.add_middleware(
         "http://localhost:9527",
         "http://localhost:3000",
         "https://web.agentcore.art",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
